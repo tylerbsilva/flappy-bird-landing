6ec321cfdc7004055de8442a66cea033e7a73ef7
Tyler Silva <tylerbsilva@gmail.com>
2015-06-09 09:31:40 -0500

    Lazy loaded header image

--- site/js/app.js
+++ site/js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  $('header')
+    .velocity({
+      opacity: 1,
+      duration: 500
+    });
   $('#headline')
     .velocity("transition.slideUpBigIn",{
       duration : 1500,
